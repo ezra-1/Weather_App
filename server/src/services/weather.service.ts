e07d2702e9ee302a83f0ffadeaf42f1b887f2366
Ezra <ezralawrence03@gmail.com>
2026-06-02 23:48:34 -0500

    feat(api): replace weather-by-city endpoint with geocoding lookup

--- server/src/services/weather.service.ts
+++ server/src/services/weather.service.ts
@@ -1,17 +1,23 @@
 import axios from "axios";
 import { env } from "../config/env";
-import { WeatherResponse } from "../types/weather.types";
 
-export const getWeatherByCity = async (city: string) => {
-  const url = `${env.WEATHER_BASE_URL}/data/2.5/weather`;
-
-  const response = await axios.get<WeatherResponse>(url, {
-    params: {
-      q: city,
-      appid: env.WEATHER_API_KEY,
-      units: "metric"
+export const getGeocoding = async (search: string) => {
+  const { data } = await axios.get(
+    `${env.WEATHER_BASE_URL}/geo/1.0/direct`,
+    {
+      params: {
+        q: search,
+        limit: 5,
+        appid: env.WEATHER_API_KEY,
+      },
     }
-  });
+  );
 
-  return response.data;
+  return data.map((location: any) => ({
+    name: location.name,
+    country: location.country,
+    state: location.state,
+    lat: location.lat,
+    lon: location.lon,
+  }));
 };
