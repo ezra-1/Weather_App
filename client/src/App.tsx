d9f14fa96c573383966182f12a562c4285698376
Ezra <ezralawrence03@gmail.com>
2026-06-03 23:46:40 -0500

    feat(app): integrate weather context and UI components
    - Add WeatherProvider
    - Add TopAppBar
    - Add FavoritesSection
    - Set up initial application layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,17 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { WeatherProvider } from "@/components/WeatherProvider";
+import { TopAppBar } from "@/components/TopAppBar";
+import { FavoritesSection } from "@/components/FavoritesSection";
+
 function App() {
-  return <ThemeProvider>Weather App</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <WeatherProvider>
+        <TopAppBar />
+        <FavoritesSection />
+      </WeatherProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
