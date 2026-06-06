5c7a95b487cf1d769b71472a1fca68ce7f180ade
Ezra <ezralawrence03@gmail.com>
2026-06-06 16:26:28 -0500

    feat(weather-service): add onecall and reverse geocoding requests

--- server/src/services/weather.service.ts
+++ server/src/services/weather.service.ts
@@ -1,16 +1,39 @@
 import axios from "axios";
 import { env } from "../config/env";
 
-export const getGeocoding = async (search: string) => {
-  const { data } = await axios.get(
-    `${env.WEATHER_BASE_URL}/geo/1.0/direct`,
-    {
-      params: {
-        q: search,
-        appid: env.WEATHER_API_KEY,
-      },
-    }
-  );
+export const getGeocoding = async (search: string, limit: number) => {
+  const { data } = await axios.get(`${env.WEATHER_BASE_URL}/geo/1.0/direct`, {
+    params: {
+      q: search,
+      limit: limit,
+      appid: env.WEATHER_API_KEY,
+    },
+  });
 
-  return data
+  return data;
+};
+
+export const getOneCall = async (lat: number, lon: number, units: string) => {
+  const { data } = await axios.get(`${env.WEATHER_BASE_URL}/data/3.0/onecall`, {
+    params: {
+      lat,
+      lon,
+      units,
+      appid: env.WEATHER_API_KEY,
+    },
+  });
+
+  return data;
+};
+
+export const getReverseGeo = async (lat: number, lon: number) => {
+  const { data } = await axios.get(`${env.WEATHER_BASE_URL}/geo/1.0/reverse`, {
+    params: {
+      lat,
+      lon,
+      appid: env.WEATHER_API_KEY,
+    },
+  });
+
+  return data;
 };
