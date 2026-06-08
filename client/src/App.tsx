ef14d6a753fc624442512153aae02b3e3f9d216f
Ezra <ezralawrence03@gmail.com>
2026-06-07 20:29:34 -0500

    feat(app): add hourly weather tabs and 7-day forecast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,9 @@ import { TopAppBar } from "@/components/TopAppBar";
 import { FavoritesSection } from "@/components/FavoritesSection";
 import { PageHeader } from "./components/PageHeader";
 import { CurrentWeatherCard } from "./components/CurrentWeatherCard";
+import { HourlyWeatherTabs } from "@/components/HourlyWeatherTabs";
 import { Map } from "@/components/Map";
+import { SevenDayForecast } from "./components/SevenDayForecast";
 
 const App = () => {
   return (
@@ -12,16 +14,14 @@ const App = () => {
       <WeatherProvider>
         <TopAppBar />
         <FavoritesSection />
-        <main className="py-4">
+        <main className="py-4 ">
           <div className="container">
-            {/* Page header */}
             <PageHeader />
-
-            {/* Current weather card & map */}
             <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
               <CurrentWeatherCard />
-
               <Map />
+              <HourlyWeatherTabs />
+              <SevenDayForecast />
             </div>
           </div>
         </main>
